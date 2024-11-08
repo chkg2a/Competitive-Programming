222f977425cce21f418630b94b2311d370f137cd
chkg2a <xshalltearvamps@gmail.com>
2024-11-08 10:51:35 +0530

    2024-11-08 10:51:35

---
+++ LeetCode/Dailies/1829.maxXORforEach.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int xorProduct = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            xorProduct = xorProduct ^ nums[i];
+        }
+        vector<int> ans(nums.size());
+
+        int mask = (1 << maximumBit) - 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans[i] = xorProduct ^ mask;
+            // remove last element
+            xorProduct = xorProduct ^ nums[nums.size() - 1 - i];
+        }
+
+        return ans;
+    }
+};
+
+int main () {
+  vector<int> nums = {0,1,1,3};
+  int maximumBit = 2;
+  Solution sol;
+  for(int i : sol.getMaximumXor(nums,maximumBit)){
+    cout << i << ' ';
+  }
+
+  return 0;
+}
